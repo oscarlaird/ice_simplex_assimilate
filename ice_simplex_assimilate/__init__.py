b1a3d689455a6161d1c1e70cb78a66cf4a98bbb7
Oscar Laird <olaird25@gmail.com>
2023-10-02 12:46:56 -0400

    coverage

--- ice_simplex_assimilate/__init__.py
+++ ice_simplex_assimilate/__init__.py
@@ -2,12 +2,10 @@ import numpy as np
 from numpy.typing import NDArray
 import warnings
 
-SIG_BITS = 31
-ONE = np.uint32(2 ** SIG_BITS)
-DELTA = np.uint32(1)
-OUTPUT_WARN_THRESHOLD = int(1e-9 * ONE)
+from simplex_assimilate.utils.quantize import quantize
+from simplex_assimilate.fixed_point import ONE
 
-THRESHOLD = 1e-6
+OUTPUT_WARN_THRESHOLD = int(1e-9 * ONE)
 
 class HeightBounds(np.ndarray):
     min_interval = 1e-7
@@ -22,7 +20,7 @@ class HeightBounds(np.ndarray):
 
     @property
     def intervals(self):
-        return zip(self[:-1], self[1:])
+        return list(zip(self[:-1], self[1:]))
 
     @classmethod
     def from_interval_widths(cls, intervals: np.ndarray):
@@ -32,8 +30,18 @@ class HeightBounds(np.ndarray):
         a = np.insert(a, 0, 0)  # insert height of zero at the beginning
         return HeightBounds(a)
 
+def check_raw_sample_legal(area: NDArray[np.float32], volume: NDArray[np.float32], height_bounds: HeightBounds):
+    """ Check that a raw sample of area and volume is legal i.e. ah₁ ≤ v ≤ ah₂ """
+    assert len(area) == len(volume) == len(height_bounds.intervals)
+    legal = True
+    legal &= np.all(area >= 0) and np.all(volume >= 0)
+    legal &= area.sum() <= 1
+    for i, interval in enumerate(height_bounds.intervals):
+        legal &= interval[0] * area[i] <= volume[i] <= interval[1] * area[i]
+    return legal
 
-def pre_process_sample(area: NDArray[np.float32], volume: NDArray[np.float32], height_bounds: HeightBounds) -> NDArray[
+
+def pre_process_sample(area: NDArray[np.float32], volume: NDArray[np.float32], height_bounds: HeightBounds, threshold=1e-6) -> NDArray[
     np.uint32]:
     """ Convert a raw sample of area and  volume to a deltized representation suitable for the simplex_assimilate package
     Input:
@@ -50,33 +58,32 @@ def pre_process_sample(area: NDArray[np.float32], volume: NDArray[np.float32], h
     5. Quantize to 32-bit fixed point
     """
     # 1. Assert that the area, volume, and height bounds have legal values (ah₁ ≤ v ≤ ah₂)
+    assert check_raw_sample_legal(area, volume, height_bounds)
     # 2. Convert to deltized form using the height bounds.
-    assert len(area) == len(volume) == len(height_bounds.intervals)
-    assert np.all(area >= 0) and np.all(volume >= 0)
-    x = []
+    x = np.zeros(2 * len(height_bounds.intervals) + 1, dtype=np.float32)
+    area_before = area.sum()
     for i, interval in enumerate(height_bounds.intervals):
         M = np.array([[1., 1., ],
                       interval])
         # M @ x = [area, volume]
-        x += list(np.linalg.inv(M) @ np.array([area[i], volume[i]]))
-        assert x[-1] >= 0 and x[
-            -2] >= 0, "Illegal area and volume causes negative mass in deltized form. Check ah₁ ≤ v ≤ ah₂."
+        l, r = np.linalg.inv(M) @ np.array([area[i], volume[i]])
         # 3. Threshold by pair. Set each pair to zero if their sum is less than 2*threshold
         # 4. For each pair, move enough mass from the larger component to make the smaller component equal to the threshold
-        if x[-1] + x[-2] < 2 * THRESHOLD:
-            x[-1] = x[-2] = 0
+        if l + r < 2 * threshold:
+            l = r = 0
         else:
-            if x[-1] < THRESHOLD:
-                x[-2] -= THRESHOLD - x[-1]
-                x[-1] = THRESHOLD
-            elif x[-2] < THRESHOLD:
-                x[-1] -= THRESHOLD - x[-2]
-                x[-2] = THRESHOLD
-    x.insert(0, max(0, 1 - sum(x)))  # first component is open water. How much isn't covered in ice?
-    x = np.array(x, dtype=np.float32)
-    x /= sum(x)  # normalize in case we have mild rounding errors
+            if l < threshold:
+                r -= threshold - l
+                l = threshold
+            elif r < threshold:
+                l -= threshold - r
+                r = threshold
+        x[2 * i + 1] = l
+        x[2 * i + 2] = r
+    x *= area_before / x.sum() # insure thresholding doesn't change the amount of ice_area
+    x[0] = max(0, 1 - x.sum())  # first component is open water. How much isn't covered in ice?
     # 5. Quantize to 32-bit fixed point
-    return quantize(x)
+    return quantize(x[None, :])[0]
 
 
 def post_process_sample(sample: NDArray[np.uint32], height_bounds: HeightBounds) -> (
@@ -96,8 +103,8 @@ def post_process_sample(sample: NDArray[np.uint32], height_bounds: HeightBounds)
     # 1. Assert that the deltized sample sums to 1
     assert sample.sum() == ONE
     # 2. Warn if any component is less than 1e-9, but greater than zero.
-    if np.any(0 < sample & sample < OUTPUT_WARN_THRESHOLD):
-        print(f"Warning: output sample contains components less than 1e-9: {sample}")
+    if np.any((0 < sample) & (sample < OUTPUT_WARN_THRESHOLD)):
+        warnings.warn(f"Warning: output sample contains components less than 1e-9: {sample}")
     # 3. Dequantize to floating point
     x = sample.astype(np.float32) / ONE
     # 4. Convert to raw form
@@ -112,32 +119,15 @@ def post_process_sample(sample: NDArray[np.uint32], height_bounds: HeightBounds)
     return area, volume
 
 
-def pre_process_ensemble(areas: NDArray[np.float32], volumes: NDArray[np.float32], height_bounds: HeightBounds) -> \
+def pre_process_ensemble(areas: NDArray[np.float32], volumes: NDArray[np.float32], height_bounds: HeightBounds, threshold=1e-6) -> \
         NDArray[np.uint32]:
     """ Apply pre_process_sample to a batch of samples """
-    vectorized_pps = np.vectorize(pre_process_sample, signature='(n),(n),()->(m)', excluded=['height_bounds'])
+    vectorized_pps = np.vectorize(pre_process_sample, signature='(n),(n)->(m)', excluded=['height_bounds', 'threshold'])
     # apply f as an aggregate function on axis 1
-    return vectorized_pps(areas, volumes, height_bounds=height_bounds)
+    return vectorized_pps(areas, volumes, height_bounds=height_bounds, threshold=threshold)
 
 
 def post_process_ensemble(ensemble: NDArray[np.uint32], height_bounds: HeightBounds) -> NDArray[np.float32]:
-    vectorized_pps = np.vectorize(post_process_sample, signature='(n),()->(m),(m)', excluded=['height_bounds'])
+    vectorized_pps = np.vectorize(post_process_sample, signature='(n)->(m),(m)', excluded=['height_bounds'])
     return vectorized_pps(ensemble, height_bounds=height_bounds)
 
-
-def quantize(float_samples: NDArray[np.float32]) -> NDArray[np.uint32]:
-    # check inputs lie on the simplex to within tolerance DELTA
-    assert float_samples.ndim == 2, "Samples must be a 2D array"
-    assert np.all(float_samples >= 0), "Samples must be non-negative"
-    assert np.all(1 - DELTA / ONE < float_samples.sum(axis=1)) and np.all(
-        float_samples.sum(axis=1) < 1 + DELTA / ONE), f"Samples must sum to 1, to tolerance {DELTA / ONE}"
-    # take cumulative sum, round to nearest quantized value, and take differences
-    cumsum = np.cumsum(float_samples, axis=1)
-    cumsum = np.insert(cumsum, 0, 0, axis=1)
-    cumsum = np.round(cumsum * ONE).astype(np.uint32)  # multiply by ONE to convert from float to uint32
-    samples = np.diff(cumsum, axis=1)
-    if not np.all((samples > 0) == (float_samples > 0)):
-        warnings.warn(f"Truncation performed in quantization. Inputs should be thresholded before quantization."
-                      f"Recommended threshold is at least 10*Δ={DELTA / ONE * 10}, preferably greater.")
-    assert (samples.sum(axis=1) == ONE).all(), "Samples do not sum to 1 after quantization"
-    return samples
---
+++ tests/process.py
@@ -0,0 +1,93 @@
+import unittest
+from ice_simplex_assimilate import *
+
+class TestProcess(unittest.TestCase):
+
+    height_bounds = HeightBounds(np.array([0.0, 2.0, 4.0]))
+
+    def test_quantize(self):
+        """ Quantized samples sum to ONE (2<<31)"""
+        samples = np.random.random((100, 11))
+        samples = samples / samples.sum(axis=1, keepdims=True)
+        samples = quantize(samples)
+        self.assertTrue(np.all(samples.sum(axis=1) == ONE))
+
+    def test_check_raw_sample_legal(self):
+        area = np.array([0.5, 0.5])
+        volume = np.array([0.5, 1.5])
+        self.assertTrue(check_raw_sample_legal(area, volume, self.height_bounds))
+        area = np.array([0.7, 0.5])
+        volume = np.array([0.7, 1.5])
+        self.assertFalse(check_raw_sample_legal(area, volume, self.height_bounds))
+        area = np.array([0.5, 0.5])
+        volume = np.array([1.5, 1.5])  # volume[0] is too large
+        self.assertFalse(check_raw_sample_legal(area, volume, self.height_bounds))
+
+    def test_pre_process_sample(self):
+        # healthy sample
+        area = np.array([0.5, 0.5])
+        volume = np.array([0.5, 1.5])
+        sample = pre_process_sample(area, volume, self.height_bounds)
+        self.assertTrue(np.all(sample == np.array([0, ONE/4, ONE/4, ONE/4, ONE/4])))
+        # healthy sample
+        area = np.array([1, 0])
+        volume = np.array([1, 0])
+        sample = pre_process_sample(area, volume, self.height_bounds)
+        self.assertTrue(np.all(sample == np.array([0, ONE/2, ONE/2, 0, 0])))
+        # mass in second interval should be thresholded
+        area = np.array([1 - 1e-7, 1e-7])
+        volume = np.array([1 - 1e-7, 3 * 1e-7])
+        sample = pre_process_sample(area, volume, self.height_bounds)
+        self.assertTrue(np.all(sample == np.array([0, ONE/2, ONE/2, 0, 0])))
+        # mass should be moved from the left side to the right side of the interval
+        area = np.array([1, 0])
+        volume = np.array([0, 0])
+        sample = pre_process_sample(area, volume, self.height_bounds)
+        self.assertTrue(np.all(sample == np.array([0, ONE - 2147, 2147, 0, 0])))
+        # open water implied
+        area = np.array([0.5, 0])
+        volume = np.array([0.5, 0])
+        sample = pre_process_sample(area, volume, self.height_bounds)
+        self.assertTrue(np.all(sample == np.array([ONE/2, ONE/4, ONE/4, 0, 0])))
+
+    def test_pre_process_ensemble(self):
+        area = np.array([[0.5, 0.5],
+                        [1, 0],
+                        [1 - 1e-7, 1e-7],
+                        [1, 0],
+                        [0.5, 0]])
+        volume = np.array([[0.5, 1.5],
+                           [1, 0],
+                           [1 - 1e-7, 3 * 1e-7],
+                           [0, 0],
+                           [0.5, 0]])
+        ensemble = pre_process_ensemble(area, volume, self.height_bounds)
+        expected_ensemble = np.array([[0, ONE/4, ONE/4, ONE/4, ONE/4],
+                                        [0, ONE/2, ONE/2, 0, 0],
+                                        [0, ONE/2, ONE/2, 0, 0],
+                                        [0, ONE - 2147, 2147, 0, 0],
+                                        [ONE/2, ONE/4, ONE/4, 0, 0]])
+        self.assertTrue(np.all(ensemble == expected_ensemble))
+
+    def test_post_process_sample(self):
+        # healthy sample
+        sample = np.array([0, ONE/4, ONE/4, ONE/4, ONE/4])
+        area, volume = post_process_sample(sample, self.height_bounds)
+        self.assertTrue(np.all(area == np.array([0.5, 0.5])))
+        self.assertTrue(np.all(volume == np.array([0.5, 1.5])))
+        # sample with all open water
+        sample = np.array([ONE, 0, 0, 0, 0])
+        area, volume = post_process_sample(sample, self.height_bounds)
+        self.assertTrue(np.all(area == np.array([0, 0])))
+        self.assertTrue(np.all(volume == np.array([0, 0])))
+
+    def test_post_process_ensemble(self):
+        ensemble = np.array([[0, ONE/4, ONE/4, ONE/4, ONE/4],
+                             [ONE, 0, 0, 0, 0]])
+        area, volume = post_process_ensemble(ensemble, self.height_bounds)
+        expected_area = np.array([[0.5, 0.5],
+                                    [0, 0]])
+        expected_volume = np.array([[0.5, 1.5],
+                                    [0, 0]])
+        self.assertTrue(np.all(area == expected_area))
+        self.assertTrue(np.all(volume == expected_volume))
